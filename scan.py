aeb025bc31b6a3c36569c0bd6314a1fdc0f399d4
venom <baibhav.229302366@muj.manipal.edu>
2025-03-19 00:21:21 +0530

    #function to get all form

--- scan.py
+++ scan.py
@@ -5,5 +5,12 @@ import sysfrom urllib.parse import urljoin
 
 s = requests.Session()
 s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
-
-
+#function to get all form
+def get_form(url):
+    soup = BeautifulSoup(s.get(url).content, "html.parser")
+    return soup.find_all("form")
+def form_details(form):
+    detailsofForm = {}
+    action = form.attrs.get("action")
+    method = form.attrs.get("method", "get")
+    inputs = []
