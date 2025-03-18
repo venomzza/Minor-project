40abfa43fa19439573ee139c4a404818284c987b
venom <baibhav.229302366@muj.manipal.edu>
2025-03-18 23:30:33 +0530

    sql injection vulnebilities

---
+++ scan.py
@@ -0,0 +1,8 @@
+
+import requests
+from bs4 import BeautifulSoup
+import sysfrom urllib.parse import urljoin
+
+s = requests.Session()
+s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+
